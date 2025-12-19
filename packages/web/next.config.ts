48ba3fb8dbffafcbcca1f3d026a0e13ebaa64709
Aharon Koss <aharonkoss@gmail.com>
2025-12-19 07:30:04 -0700

    updated next to version 16

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['shared'],
+   // Disable Turbopack to avoid OneDrive sync issues
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
